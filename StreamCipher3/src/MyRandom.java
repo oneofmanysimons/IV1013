8a9c5f7e343a171bd1e3fe9148c7039c62260c53
oneofmanysimons <114488646+oneofmanysimons@users.noreply.github.com>
2024-11-20 14:19:46 +0100

    Add files via upload

---
+++ StreamCipher3/src/MyRandom.java
@@ -0,0 +1,31 @@
+import java.math.BigInteger;
+import java.util.Random;;
+
+public class MyRandom extends Random {
+    long seed;
+    int m = 97;
+    int a = 5;
+    int b = 0;
+    public MyRandom(){
+        super();
+    }
+    public MyRandom(BigInteger seed){
+        super();
+        BigInteger[] split = seed.divideAndRemainder(BigInteger.valueOf(m));
+        setSeed(split[1].longValue());
+    }
+    @Override
+    public void setSeed(long seed){
+        this.seed = seed;
+    }
+    @Override
+    public int next(int bits){
+        setSeed((a*seed + b) % m);
+        long max = (long) (Math.pow(2,bits)-1);
+        //does not work if seed >> max because stack die
+        if (this.seed > max){
+            return nextInt(bits);
+        }
+        return (int) this.seed;
+    }
+}
---
+++ StreamCipher3/src/com/company/StreamCipher.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.math.BigInteger;
+import com.company.MyRandom;
+
+public class StreamCipher {
+
+    public static void main(String[] args) throws Exception {
+        //handeling the input
+        if (args.length != 6){
+            throw new Exception("Format: --key <key> --in <infile> --out <outfile>");
+        }
+        //Global variables
+        BigInteger key = null;
+        String infile = "";
+        String outfile = "";
+        //Handeling input
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("--key")){
+                key = new BigInteger(args[i+1]);
+            }
+            if (args[i].equals("--in")){
+                infile = args[i+1];
+            }
+            if (args[i].equals("--out")){
+                outfile = args[i+1];
+            }
+        }
+        //check for that an infile exist
+        File inputFile = new File(infile);
+        if (!inputFile.exists()){
+            throw new Exception("There need to exist an input-file.");
+        }
+        //checking if outputFile exist
+        File outFile = new File(outfile);
+        if (!outFile.createNewFile()) {
+            outFile.delete();
+            outFile.createNewFile();
+        }
+        //Send message byte per byte
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outFile);
+        MyRandom random = new MyRandom(key.longValue());
+        int part = 0;
+        //handeling the message
+        while ((part = inputStream.read()) != -1) {
+            part = part ^ random.next(8);
+            outputStream.write((byte)part);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+}
